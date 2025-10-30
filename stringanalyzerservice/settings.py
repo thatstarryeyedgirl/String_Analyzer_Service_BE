4465380ed423a16d73d59c4654160c9ebd926df1
Valerie <okorovaleriechikanyima@gmail.com>
2025-10-30 12:56:23 +0100

    Updated String Analyzer Service project to enable deployment to render

--- stringanalyzerservice/settings.py
+++ stringanalyzerservice/settings.py
@@ -132,3 +132,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
+}
--- stringanalyzerservice/urls.py
+++ stringanalyzerservice/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def root_view(request):
+    return JsonResponse({"message": "String Analyzer Service API", "status": "running"})
 
 urlpatterns = [
+    path('', root_view, name='root'),
     path('admin/', admin.site.urls),
     path('string-analyzer/', include('stringanalyzerservice_app.urls'))
 ]
