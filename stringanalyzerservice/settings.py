456868250304e03a7e3cac7919797f111f1e97c9
Valerie <okorovaleriechikanyima@gmail.com>
2025-10-30 12:43:16 +0100

    Updated String Analyzer Service project to enable deployment to render

--- stringanalyzerservice/settings.py
+++ stringanalyzerservice/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'HOST': os.getenv('DB_HOST'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),    
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
