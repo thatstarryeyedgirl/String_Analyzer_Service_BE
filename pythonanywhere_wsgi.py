42af29127a11f89e49fcd8215fd4ad24b7816549
Valerie <okorovaleriechikanyima@gmail.com>
2025-10-30 16:25:53 +0100

    Updated String Analyzer Service project to enable deployment to render

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -5,7 +5,7 @@ import sys
 
 # Add your project directory to the sys.path
 # Replace 'yourusername' with your actual PythonAnywhere username
-path = '/home/yourusername/String-Analyzer-Service'
+path = '/home/padimi/String-Analyzer-Service/String_Analyzer_Service_BE'
 if path not in sys.path:
     sys.path.insert(0, path)
 
@@ -14,7 +14,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'stringanalyzerservice.settings'
 
 # Load environment variables from .env file
 from dotenv import load_dotenv
-load_dotenv('/home/yourusername/String-Analyzer-Service/.env')
+load_dotenv('/home/padimi/String-Analyzer-Service/String_Analyzer_Service_BE/.env')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
