29e9de50014a73a31a8b36cdbb4bec5b0181fcae
Valerie <okorovaleriechikanyima@gmail.com>
2025-10-31 12:24:48 +0100

    Updated String Analyzer Service project to enable deployment to render

--- stringanalyzerservice/settings.py
+++ stringanalyzerservice/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['padimi.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Add your PythonAnywhere username domain above
 # Replace 'yourusername' with your actual PythonAnywhere username
